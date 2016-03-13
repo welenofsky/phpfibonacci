cfb267ed1f88b0142fe29fda98809c39497fdf6b
Justin Welenofsky <welenofsky@gmail.com>
2016-03-13 16:18:32 -0700

    Added standard recursion fibonacci

--- fibonacci.php
+++ fibonacci.php
@@ -1,10 +1,18 @@
 <?php
-$generator = fibonacci();
+// Using Generators
+
+$generator = fibonacci_gen();
 foreach ($generator as $i) {
 	echo "$i\n";
 }
 
-function fibonacci() {
+
+// Using recursion
+fibonacci();
+
+echo "\nMemory Usage: " . memory_get_usage();
+
+function fibonacci_gen() {
 	if (version_compare(phpversion(), '5.5.0', '>=')) {
 		// 5.4.0 supports short array syntax
 		// 5.5.0 supports generators (yield)
@@ -15,4 +23,13 @@ function fibonacci() {
 		} while ($a !== INF);
 	} else { return; }
 }
-?>
+
+function fibonacci($a = 0, $b = 1, $end = INF) {
+	// This is the more portable solution
+	// Should be supported in PHP 4, PHP 5, PHP 7+
+	echo "$a\n";
+	list($a, $b) = array($b, $a+$b);
+	if ($a < $end + 1) {
+		fibonacci($a, $b, $end);
+	}
+}
