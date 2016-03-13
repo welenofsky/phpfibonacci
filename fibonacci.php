a53d6cdf3468aec43c470cfccc73c9e54ab4b891
Justin Welenofsky <welenofsky@gmail.com>
2016-03-13 13:14:02 -0700

    init

---
+++ fibonacci.php
@@ -0,0 +1,18 @@
+<?php
+$generator = fibonacci();
+foreach ($generator as $i) {
+	echo "$i\n";
+}
+
+function fibonacci() {
+	if (version_compare(phpversion(), '5.5.0', '>')) {
+		// 5.4.0 supports short array syntax
+		// 5.5.0 supports generators (yield)
+		list($a, $b) = [0, 1];
+		do {
+			yield $a;
+			list($a, $b) = [$b, $a+$b];
+		} while ($a !== INF);
+	} else { return; }
+}
+?>
